50daed8a09713b7b31baad90d95afa50edac6f85
senhorN <juniornicolas529@gmail.com>
2024-02-25 14:21:35 -0300

    DragonForce

---
+++ DragonForcev3/Modules/Commands.cs
@@ -0,0 +1,41 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+
+namespace DragonForcev3.Modules
+{
+    public  class Commands :  ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task Ping() {
+            await ReplyAsync("Pong");
+        }
+
+        //comando de banir
+        [Command("ban")]
+        [RequireUserPermission(GuildPermission.BanMembers, ErrorMessage = "Você não tem permissão ``TOMA-LHE BAN``!")]
+        public async Task BanMember(IGuildUser user = null, [Remainder] string reason = null) {
+            
+            if (user == null) await ReplyAsync("Por favor, especifique o usuário!");
+
+            if (reason == null) reason = "Não especificado";
+
+            await Context.Guild.AddBanAsync(user, 1, reason);
+
+            var EmbedBuilder = new EmbedBuilder()
+                .WithDescription($":white_check_mark: {user.Mention} foi banidoz\n por {reason}")
+                .WithFooter(footer => {
+                    footer
+                    .WithText("Usuário banido log")
+                    .WithIconUrl("https://i.imgur.com.6Bi17B3.png");
+                });
+            Embed embed = EmbedBuilder.Build();
+            await ReplyAsync(embed: embed);
+        }
+    }
+}
---
+++ DragonForcev3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DragonForcev3
+{
+    internal class Program
+    {
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+        
+        #region metodos privados
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private  IServiceProvider _services;
+        #endregion
+
+
+        public async Task RunBotAsync() { 
+            _client = new DiscordSocketClient();
+            _commands = new CommandService();
+            _services = new ServiceCollection()
+                .AddSingleton(_client)
+                .AddSingleton(_commands)
+                .BuildServiceProvider();
+
+            string token = "MTIxMTEwNTU2NTIJNSDUJND65D893NINDUNOKQLKEDcsj0KOc1rP8MfPr4vvdoBLQMc9K3egsa4Y";
+
+            _client.Log += _client_Log;
+
+            await ResgisterCommandsAsync();
+
+            await _client.LoginAsync(TokenType.Bot, token);
+
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+
+        }
+
+        private Task _client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+
+            return Task.CompletedTask;
+        }
+
+        public async Task ResgisterCommandsAsync() {
+
+            _client.MessageReceived += HandleCommandAsync;
+
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        private async  Task HandleCommandAsync(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new SocketCommandContext(_client, message);
+            
+            if (message.Author.IsBot) return;
+
+            int argsPos = 0;
+            if (message.HasStringPrefix("!", ref argsPos)) {
+                
+                var result = await _commands.ExecuteAsync(context, argsPos, _services);
+
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+
+                if (result.Error.Equals(CommandError.UnmetPrecondition)) await message.Channel.SendMessageAsync(result.ErrorReason);
+            }
+        }
+    }
+}
